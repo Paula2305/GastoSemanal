8f499feecada0cb90392f9561230c296bc6d778d
Paula2305 <polimoreno1976@gmail.com>
2023-11-19 21:26:06 -0300

    Creacion de presupuesto

--- js/app.js
+++ js/app.js
@@ -12,16 +12,39 @@ function eventListeners(){
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 
-// Clasees
+// Clases
+
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
+
+const ui = new UI();
+
+let presupuesto ;
 
 // Funciones 
 
 function preguntarPresupuesto(){
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
-    console.log(presupuestoUsuario);
+    // console.log(presupuestoUsuario);
 
     // Si el prompt esta vacio/no se carga el campo/es una letra y se apreta aceptar/cancelar, se vuelve a recargar la pagina 
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
         window.location.reload();
     }
+
+    // Presupuesto valido 
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 }
+
+
+
